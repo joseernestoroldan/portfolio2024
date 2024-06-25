42a514721d256721d52515c79115bab559c18c1f
Jose <joseernestoroldan@gmail.com>
2024-06-25 13:23:13 -0400

    HomeConfigure | HomeLinks | Headings

---
+++ components/home/HomeLinks.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+
+const HomeLinks = () => {
+  return (
+    <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <Link
+        href="#"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-2xl font-semibold">
+          About{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Up front and clear: this section is all about me.
+        </p>
+      </Link>
+
+      <Link
+        href="#"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-2xl font-semibold">
+          Works{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Curious about what I do? Take a peek!
+        </p>
+      </Link>
+
+      <Link
+        href="#"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-2xl font-semibold">
+          Contact{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          We will be glad to make your dream project come true.
+        </p>
+      </Link>
+
+      <Link
+        href="#"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-2xl font-semibold">
+          Learn{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+          Let us lear together about what we love to do.
+        </p>
+      </Link>
+    </div>
+  );
+};
+
+export default HomeLinks;
